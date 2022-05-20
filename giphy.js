a9a2e5b7c01a726aee6a4a8063216646946d5371
SlenderSnell <dtsnell555@gmail.com>
2022-05-20 18:28:34 -0400

    added api code to javascript

--- giphy.js
+++ giphy.js
@@ -1,1 +1,11 @@
-console.log('FUCK');
+let apikey = "BJMBunRucto16Ju2jTjBVXAQCaH414VW";
+document.addEventListener("DOMContentLoaded", init);
+function init(){
+    document.getElementById("goButton").addEventListener('click', e => {
+        e.preventDefault();
+        let url = `http://api.giphy.com/v1/gifs/search?api=${apikey}&limit=8&q=`;
+        let str = document.getElementById("search").value.trim();
+        url = url.concat(str);
+        console.log(url);
+    })
+}
