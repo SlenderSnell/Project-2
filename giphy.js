5d2bed03fe35554afacf8d5b83cebfa1e3bccc47
SlenderSnell <dtsnell555@gmail.com>
2022-05-24 20:31:15 -0400

    updated css and made the js finally work

--- giphy.js
+++ giphy.js
@@ -1,11 +1,22 @@
-let apikey = "BJMBunRucto16Ju2jTjBVXAQCaH414VW";
-document.addEventListener("DOMContentLoaded", init);
-function init(){
-    document.getElementById("goButton").addEventListener('click', e => {
-        e.preventDefault();
-        let url = `http://api.giphy.com/v1/gifs/search?api=${apikey}&limit=8&q=`;
-        let str = document.getElementById("search").value.trim();
-        url = url.concat(str);
-        console.log(url);
+const apikey = "BJMBunRucto16Ju2jTjBVXAQCaH414VW";
+const button = document.getElementById('goButton');
+const search = document.getElementById('search');
+const out = document.getElementById('out');
+
+button.addEventListener('click', e => {
+    let input = search.value;
+    let url = `http://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${input}&limit=8`;
+    fetch(url).then(function(res){
+        return res.json()
+    }).then(function(info){
+        let gifArray = info.data
+        for(let i=0;i<gifArray.length;i++){
+            let gifURL = gifArray[i].images.fixed_height_small.url;
+            let newImg = document.createElement('img');
+            newImg.setAttribute('src', gifURL);
+            out.appendChild(newImg);
+        }
+    }).catch(function(error){
+        console.log(error)
     })
-}
+})
